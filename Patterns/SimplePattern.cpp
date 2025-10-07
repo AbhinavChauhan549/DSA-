1b97e8bab4f0b1e9a86113ff45b600d9b050ff5e
Abhinav Chauhan <officialabhi549@gmail.com>
2025-10-07 21:58:56 +0530

    16 pattern (beginner to advanced)

--- Patterns/SimplePattern.cpp
+++ Patterns/SimplePattern.cpp
@@ -516,21 +516,21 @@ int main()
 
   */
 
-  int size = 2 * n - 1;
+  // int size = 2 * n - 1;
 
-  for (int i = 0; i < size; i++)
-  {
-    for (int j = 0; j < size; j++)
-    {
-      int top = i;
-      int left = j;
-      int right = size - 1 - j;
-      int bottom = size - 1 - i;
+  // for (int i = 0; i < size; i++)
+  // {
+  //   for (int j = 0; j < size; j++)
+  //   {
+  //     int top = i;
+  //     int left = j;
+  //     int right = size - 1 - j;
+  //     int bottom = size - 1 - i;
 
-      int minDist = min(min(top, bottom), min(left, right));
-      cout << n - minDist << " ";
-    }
-    cout << endl;
-  }
+  //     int minDist = min(min(top, bottom), min(left, right));
+  //     cout << n - minDist << " ";
+  //   }
+  //   cout << endl;
+  // }
   return 0;
 }
